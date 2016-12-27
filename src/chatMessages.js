3f10b3bdfd1c908939cad00f8e49c4ac374bd0e5
Juan Garnica <jupegarnica@gmail.com>
2016-12-28 00:47:11 +0100

    redux isolation

--- src/chatMessages.js
+++ src/chatMessages.js
@@ -1,37 +1,43 @@
 import React, {Component} from 'react';
 import ChatMessage from './chatMessage';
-let intervalID;
-export default class ChatMessages extends Component {
+import {connect} from 'react-redux';
+import {stateToProps, dispatchToProps, startAutoScrollAction, stopAutoScrollAction} from './redux';
+
+class ChatMessagesComp extends Component {
     constructor(props) {
         super();
-        window.addEventListener("resize", this.runAutoScrollDown.bind(this));
+        window.addEventListener("resize", () => {
+            if (!this.props.state.isAutoScrolling) {
+                this.props.dispatch(startAutoScrollAction(this.chatMessagesElement));
+                setTimeout(() => {
+                    this.props.dispatch(stopAutoScrollAction());
+                },1000)
+            }
+        });
     }
-    componentDidUpdate(){
-        this.runAutoScrollDown();
-    }
-    runAutoScrollDown(){
-        const element = this.autoScrollDown;
-        element.scrollTop = element.scrollHeight;
-    }
-    onUpdateText(){
-        if (!intervalID) {
-            this.runAutoScrollDown();
-            intervalID = setInterval(this.runAutoScrollDown.bind(this), 200);
+    onUpdateText() {
+        if (!this.props.state.isAutoScrolling) {
+            this.props.dispatch(startAutoScrollAction(this.chatMessagesElement));
         }
     }
-    onTypingDone(){
-        clearInterval(intervalID);
-        intervalID = null;
+    onTypingDone() {
+        if (this.props.state.isAutoScrolling) {
+            this.props.dispatch(stopAutoScrollAction());
+        }
     }
     render() {
-        const lastMessange = this.props.messages.length -1;
-        const history = this.props.messages.map((el, i) => <ChatMessage animate={i === lastMessange} key={i} onTypingDone={this.onTypingDone.bind(this)} onUpdateText={this.onUpdateText.bind(this)} content={el.content} me={el.user === 'me'}/>);
+        const lastMessange = this.props.state.messagesHistory.length - 1;
+        const history = this.props.state.messagesHistory.map((el, i) => <ChatMessage animate={i === lastMessange} key={i} onTypingDone={this.onTypingDone.bind(this)} onUpdateText={this.onUpdateText.bind(this)} content={el.content} me={el.user === 'me'}/>);
         return (
-            <div className="chat-messages" ref={(input) => { this.autoScrollDown = input }}>
+            <div className="chat-messages" ref={(input) => {
+                this.chatMessagesElement = input
+            }}>
                 <div className="chat-messages-wraper">
+                    {history}
                     {history}
                 </div>
             </div>
         );
     }
 }
+export default connect(stateToProps, (dispatchToProps))(ChatMessagesComp)
--- src/chatShell.js
+++ src/chatShell.js
@@ -1,68 +1,16 @@
-import React, {Component} from 'react'
-import {createStore, applyMiddleware} from 'redux'
-import {Provider} from 'react-redux'
-import Logger from 'redux-logger'
-import {Chat} from './chat.js'
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import Chat from './chat.js';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; //material-ui dependency
+import {store,changeThemeAction} from './redux.js';
 
-const initialmessagesHistory = [
-    {
-        type: 'text',
-        content: 'hola',
-        user: 'me'
-    }, {
-        type: 'text',
-        content: 'LoremLorem ipsum dolor sit amet, consectetur adipisicing elit, ',
-        user: 'john doe'
-    }, {
-        type: 'text',
-        content: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        user: 'me'
-    }, {
-        type: 'text',
-        content: ':)',
-        user: 'john'
-    }
-];
-const initialState = {
-    messagesHistory: initialmessagesHistory,
-    theme: 'black'
-}
+// triggers
+//changeThemeAction
+let search = window.location.search.match(/bg=[^&]*/);
 
-// Reducer:  Pure function and inmutable state aproach
-function chatReducer(state = initialState, action) {
-    switch (action.type) {
-        case 'sendMessage':
-            return {
-                ...state,
-                messagesHistory: [
-                    ...state.messagesHistory,
-                    action.payload
-                ]
-            }
-        case 'changeTheme':
-            document.body.classList.add(action.payload);
-            return {
-                ...state,
-                theme: action.payload
-            }
-        default:
-            return state
-    }
-}
+let theme = search && search.length ? search[0].split('bg=').join('').split('&').join('') : '';
+theme && store.dispatch(changeThemeAction(theme));
 
-// Store
-const store = createStore(chatReducer, initialState, applyMiddleware(Logger()))
-
-//changeTheme trigger
-
-//action
-const changeTheme = (theme) => {
-    return {type: 'changeTheme', payload: theme};
-}
-
-let theme = window.location.search.match(/bg=[^&]*/)[0].split('bg=').join('').split('&').join('');
-store.dispatch(changeTheme(theme))
 
 // React component
 class ChatShellComp extends Component {
